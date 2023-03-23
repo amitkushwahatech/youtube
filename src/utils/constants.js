f846df89ffbce5a0441c98ea43d757b3448ffc37
amitkushwahatech <amitkushwahatech@gmail.com>
2023-03-23 17:53:28 +0530

    first commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -35,8 +35,8 @@ export const categories = [
 ];
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoChannelUrl = '/channel/UCFYQv84mH98MI0cEO3qwqBQ';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
+export const demoChannelTitle = 'Vishnu Code Fry';
 export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
